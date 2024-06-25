6cd2fffb52778ebf3b57dedc8433089e5b7320da
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-06-25 01:22:45 -0300

    added basic program

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import keyboard
+import threading
+from playsound import playsound
+
+location = os.path.realpath(os.path.join(
+            os.getcwd(), os.path.dirname(__file__)))
+
+print(location)
+
+def play_sound(sound_file):
+    try:
+        playsound(sound_file)
+    except:
+        pass
+    
+def handle_keypress(char):
+    sound_folder = "\\audio\\english"
+    sound_file = f"{location}{sound_folder}\\{char}.wav"
+    
+    # Correctly create a thread to play the sound
+    sound_thread = threading.Thread(target=play_sound, args=(sound_file,))
+    sound_thread.start()
+    
+def on_key_press(event):
+    print(event.name)
+    char = event.name.lower()
+    handle_keypress(char)
+
+keyboard.on_press(on_key_press)
+keyboard.wait() 
