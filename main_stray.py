91ab77b454ea56b93ed788451911d2b8c180dada
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-07-20 14:14:10 -0300

    Added radio buttons functionality

--- main_stray.py
+++ main_stray.py
@@ -6,11 +6,13 @@ class keysoundsTray:
     def __init__(self):
         super().__init__()
 
-        self.active_state = True
-        self.voice_state = True
         self.lang_list = [i for i in listdir(f"{getcwd()}/media/audio") if i[0] != "~"]
         self.voice_list = [i for i in listdir(f"{getcwd()}/media/audio/pt-br/") if i[0] != "~"]
-        self.img_path = f"{getcwd()}/media/icon/neutral.png"
+        self._path = f"{getcwd()}/media/icon/neutral.png"
+
+        self.active_state = True
+        self.active_lang = "None"
+        self.active_voice = "None"
 
         if len(self.voice_list) <= 0:
             raise Exception("None voices where found. Please add one or reinstall the program.")
@@ -33,23 +35,41 @@ class keysoundsTray:
     
     def keysounds_menu(self):
         return Menu(
-            MenuItem("Activate", self.toggle_active, checked= lambda item: self.active_state),
+            MenuItem("Activate", self.toggle_check, checked= lambda item: self.active_state),
             # LANG SUBMENU
             MenuItem("Change Language", Menu(
-                lambda: [MenuItem(lang, lambda x, lang=lang: self.toggle_voice(lang), checked= lambda x: False) for lang in self.lang_list]
+                lambda: [MenuItem(
+                      # name       x=icon class        command when clicked                        check if curent item is selected      radio style for button
+                        lang, lambda x, lang=lang: self.toggle_radio(lang.text), checked= lambda x, lang=lang: self.radio_check(lang), radio=True
+                        ) for lang in self.lang_list] # in a loop of all languages
             )),
             # VOICES SUBMENU
             MenuItem("Change Voices", Menu(
-                lambda: [MenuItem(voice, lambda x, voice=voice: self.toggle_voice(voice), checked= lambda x: False) for voice in self.voice_list]
+                lambda: [MenuItem(
+                        voice, lambda x, voice=voice: self.toggle_radio(voice.text), checked= lambda x, voice=voice: self.radio_check(voice), radio=True
+                        ) for voice in self.voice_list]
             )),
             MenuItem("Exit", lambda icon: icon.stop())
         )
 
-    def toggle_active(self, icon, item):
+    def toggle_check(self, icon, item):
         self.active_state = not item.checked
     
-    def toggle_voice(self, voice):
-        print(voice)
+    # RADIO BUTTONS HANDLER
+    def toggle_radio(self, item):
+        if item in self.voice_list:
+            self.active_voice = item
+        elif item in self.lang_list:
+            self.active_lang = item
+    
+    def radio_check(self, item):
+        # Could be in one if statemant but I prefer this way, better organized
+        if item in self.voice_list and item == self.active_voice:
+            return True
+        elif item in self.lang_list and item == self.active_lang:
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
