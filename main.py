d70fa5f7aa55a8fa487ef06fc9b5d9a122ebedd3
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-07-20 14:47:29 -0300

    Added main integration with its sys tray

--- main.py
+++ main.py
@@ -3,35 +3,57 @@ import keyboard
 import threading
 from playsound import playsound
 
-location = os.path.realpath(os.path.join(
-            os.getcwd(), os.path.dirname(__file__)))
+from main_stray import keysoundsTray
 
-print(location)
+class keysounds:
+    def __init__(self):
+        super().__init__()
 
-def play_sound(sound_file):
-    try:
-        playsound(sound_file)
-    except:
-        pass
-    
-def handle_keypress(char):
-    sound_folder = "\\audio\\pt-br"
+        self.location = os.path.realpath(os.path.join(
+                    os.getcwd(), os.path.dirname(__file__)))
+        
+        self.tray = keysoundsTray()
+        
+        keyboard.on_press(self.on_key_press)
+        
+        print("running...")
+        self.tray.icon.run()
+        # when the icons kills itself
 
-    if char == "?":
-        char = "interrogacao"
-    elif char == "!":
-        char = "exclamacao"
+        # TODO: BYE SOUND WHEN EXITS
+        print("exit")
+        exit()
 
-    sound_file = f"{location}{sound_folder}\\{char}.wav"
-    
-    # Correctly create a thread to play the sound
-    sound_thread = threading.Thread(target=play_sound, args=(sound_file,))
-    sound_thread.start()
-    
-def on_key_press(event):
-    print(event.name)
-    char = event.name.lower()
-    handle_keypress(char)
+    def play_sound(self, sound_file):
+        try:
+            playsound(sound_file)
+        except:
+            print(f"File not found: '.{sound_file[len(self.location):]}'")
+            pass
+        
+    def handle_keypress(self, char):
+        sound_folder = f"\\media\\audio\\{self.tray.active_voice}"
 
-keyboard.on_press(on_key_press)
-keyboard.wait() 
+        if char == "?":
+            char = "interrogacao"
+        elif char == "!":
+            char = "exclamacao"
+
+        sound_file = f"{self.location}{sound_folder}\\{char}.wav"
+        
+        print(char)
+
+        # Correctly create a thread to play the sound
+        sound_thread = threading.Thread(target=self.play_sound, args=(sound_file,))
+        sound_thread.start()
+        
+    def on_key_press(self, event):
+        # do nothing when inactive.
+        if self.tray.active_state == False:
+            return
+        
+        char = event.name.lower()
+        self.handle_keypress(char)
+
+if __name__ == "__main__":
+    keysounds_tray = keysounds()
